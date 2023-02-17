f13b5f0eabf09a19e2b2bf5c4e840c0e6e8d24f1
bonasco10 <bonasco10@gmail.com>
2023-02-17 10:25:09 -0800

    printf2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,11 +4,9 @@
  * main - the main function
  * printf - the command
  * Return: always 0
- */ 
-
-
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}	
+}
