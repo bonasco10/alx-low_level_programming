01f09df8aa6714eed28c8535ecb92e6272f8c6db
bonasco10 <bonasco10@gmail.com>
2023-02-17 14:57:51 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - main function
  *
- * Return: should be 1
+ * Return: 1 (successful)
  */
-
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
