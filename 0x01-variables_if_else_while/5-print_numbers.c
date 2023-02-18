c296e066b726b2095680dc8261d4dd4be362815d
bonasco10 <bonasco10@gmail.com>
2023-02-17 18:28:35 -0800

    numbers1

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 0 ; x < 10 ; x++)
+	{
+		printf("%d", x);
+	}
+	printf("\n");
+	return (0);
+}
