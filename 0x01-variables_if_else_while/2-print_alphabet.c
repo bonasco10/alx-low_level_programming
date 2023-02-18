8e65b3fc510d1b237d0a0e88f3d6da4a1fde8239
bonasco10 <bonasco10@gmail.com>
2023-02-17 17:54:43 -0800

    alphabets1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
