af64b63c2a673a3525dd60310c9147080a404c7f
bonasco10 <bonasco10@gmail.com>
2023-02-17 19:29:41 -0800

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Is the main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0 ; x < 10 ; x++)
+	{
+		if (x == 9)
+			putchar(x + '0');
+		else
+		{
+			putchar(x + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
