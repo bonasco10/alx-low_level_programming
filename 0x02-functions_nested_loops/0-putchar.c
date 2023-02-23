08be3e19c4b725c19e062ddb649bff896e8db796
bonasco10 <bonasco10@gmail.com>
2023-02-23 09:03:20 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <main.h>
+/**
+ * main -  the main function
+ * _putchar - writes a single character to stdout
+ *
+ * Return: 0 on success.
+ */
+
+int main(void)
+{
+        _putchar('_');
+        _putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
+        return (0);
+}
