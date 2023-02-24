8835b69c0e31f10adbb6742d10b1f54a2209e0b9
bonasco10 <bonasco10@gmail.com>
2023-02-24 07:05:03 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - absolute value of an integer
+ *
+ * @i: parameter to be checked.
+ * Return: always i.
+ */
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		i = -(i);
+	}
+	else if (i >= 0)
+	{
+		i = i;
+	}
+	return (i);
+}
